56d44dec82806884af75b318da7a91d46d3997ea
Ashok381 <ashok706157@gmail.com>
2026-05-29 21:24:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let back =document.getElementById('all') ; 
+// back.style.backgroundColor = '#1e293b'
+// let listcolor = ['#0f176a' ,'#0f172a',
+// '#1e293b' ,
+// '#312e81' ,
+// '#0f766e' ,
+// '#1d4ed8']
+// setInterval(()=>{
+//     back.style.backgroundColor = listcolor[Math.floor(Math.random()*(listcolor.length-1))]
+// } , 10000) 
